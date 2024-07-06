e7dc80e07bf6dc88632867d6edfb7316e0bb6f89
Shubham Gupta <shubham.gupta@triomics.in>
2024-07-07 05:21:50 +0530

    WIP: Update API calls and contract functios

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,16 +3,26 @@ import React, { ReactNode } from "react";
 type ButtonProps = {
   endIcon?: ReactNode;
   loading?: boolean;
+  tooltipText?: string;
 } & React.ComponentPropsWithoutRef<"button">;
 
-export function Button({ children, endIcon, loading, ...buttonProps }: ButtonProps) {
+export function Button({
+  children,
+  endIcon,
+  tooltipText,
+  loading,
+  ...buttonProps
+}: ButtonProps) {
   return (
-    <div className="button">
-      <button {...buttonProps}>
-        {children}
-        {endIcon ? endIcon : null}
-        {loading ? <div className="loader" /> : null}
-      </button>
+    <div className="tooltip">
+      {tooltipText ? <span className="tooltiptext">{tooltipText}</span>: null}
+      <div className="button">
+        <button {...buttonProps}>
+          {children}
+          {endIcon ? endIcon : null}
+          {loading ? <div className="loader" /> : null}
+        </button>
+      </div>
     </div>
   );
 }
