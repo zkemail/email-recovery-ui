2e39df76328370888ea5061609c7cb5c36201dfd
wshino <wshinohara@gmail.com>
2024-09-12 23:43:40 +0900

    WIP

--- src/utils/recoveryDataUtils.ts
+++ src/utils/recoveryDataUtils.ts
@@ -1,11 +1,10 @@
 import { encodeFunctionData, encodePacked } from 'viem'
 import { abi as safeAbi } from "../abi/Safe.json"
+import { AbiCoder, ethers } from 'ethers';
 
 export const getRecoveryData = (validator: string, recoveryCalldata: string) => {
-    return encodePacked(
-        ['address', 'bytes'],
-        [validator, recoveryCalldata]
-    );
+    const defaultAbiCoder = AbiCoder.defaultAbiCoder();
+    return defaultAbiCoder.encode(['address', 'bytes'], [validator, recoveryCalldata]);
 };
 
 export const getRecoveryCallData = (prevOwner: string, oldOwner: string, newOwner: string) => {
