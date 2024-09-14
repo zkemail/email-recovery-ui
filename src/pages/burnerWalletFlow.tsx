462c27837ec2a84fc498b1796341cb3e9b55d09e
Shubham Gupta <shubham.gupta2956@gmail.com>
2024-09-14 21:45:57 +0530

    Refactoring changes and bug fixes
    - Add error message on 502
    - Fix refresh of page on polling

--- src/pages/burnerWalletFlow.tsx
+++ src/pages/burnerWalletFlow.tsx
@@ -1,33 +1,17 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import { BurnerWalletProvider } from "../providers/BurnerWalletProvider";
-import { createBurnerSafeConfig } from "../providers/burnerWalletConfig";
 import { StepsContext } from "../App";
 import { STEPS } from "../constants";
-import SafeModuleRecovery from "../components/burnerWallet/SafeModuleRecovery";
 import GuardianSetup from "../components/burnerWallet/GuardianSetup";
 import RequestedRecoveries from "../components/burnerWallet/RequestedRecoveries";
 import TriggerAccountRecovery from "../components/TriggerAccountRecovery";
-import Loader from "../components/Loader";
-import toast from "react-hot-toast";
-import { install, run } from "../utils/burnerWalletUtils";
-import ConnectBurnerWallet from "./burnerWalletFlow/ConnectBurnerWallet";
+import { Web3Provider } from "../providers/Web3Provider";
 
 const BurnerWalletFlow = () => {
   const stepsContext = useContext(StepsContext);
-  const [isBurnerWalletCreating, setIsBurnerWalletCreating] = useState(false);
-  const [burnerWalletConfig, setBurnerWalletConfig] = useState();
-
-  // useEffect(() => {
-  //   run();
-  //   install()
-  // }, []);
-
-  console.log(stepsContext?.step, STEPS);
 
   const renderBody = () => {
     switch (stepsContext?.step) {
-      // case STEPS.CONNECT_WALLETS:
-      //   return <ConnectBurnerWallet />;
       case STEPS.REQUEST_GUARDIAN:
         return <GuardianSetup />;
       case STEPS.REQUESTED_RECOVERIES:
@@ -39,21 +23,10 @@ const BurnerWalletFlow = () => {
     }
   };
 
-  if (isBurnerWalletCreating) {
-    return (
-      <div className="app">
-        <Loader />
-      </div>
-    );
-  }
-  // if (!burnerWalletConfig) {
-  //   return <div className="app">Could not configure burner wallet</div>;
-  // }
-
   return (
-    <BurnerWalletProvider config={burnerWalletConfig}>
+    <Web3Provider>
       <div className="app">{renderBody()}</div>
-    </BurnerWalletProvider>
+    </Web3Provider>
   );
 };
 
